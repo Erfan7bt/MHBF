29a696e7f835439df666c928563d47003f74c81a
dobulexyz <rwgelston@proton.me>
2023-07-27 15:23:15 +0200

    Added functions from PlottingFunctions to plotter.py

--- Projects/plotter.py
+++ Projects/plotter.py
@@ -246,3 +246,165 @@ def visWP(model):
         ax[1].spines['right'].set_color('none')
         ax[1].spines['top'].set_color('none')
         # Show ticks in the left and lower axes only
+
+
+def plot_input_range(model, f_in_vec, in_params, 
+                     num_repeat=10,
+                     alpha=0,
+                     figsize=(8,6),
+                     cmap_str='turbo',
+                     background='grey',
+                     linewidth=2.0):
+    
+    if alpha==0:
+        alpha = 1/num_repeat
+            
+    cmap = plt.cm.get_cmap(cmap_str, len(in_params))
+    tmp_u, tmp_y = f_in_vec(in_params[0])
+ 
+    plt.figure(figsize=figsize)
+    plt.xlabel("Time step")
+    plt.ylabel("Network output (z)")
+    plt.title("Each input repeated {} times".format(num_repeat))
+    
+    #ax = plt.axes()
+    #ax.set_facecolor("grey")
+
+    for idx, par in enumerate(in_params):
+        
+        data = np.zeros((num_repeat, tmp_u.shape[0]))
+        
+        for i in range(num_repeat):
+            data[i], y = f_in_vec(par)
+            
+        data = torch.Tensor(data)
+            
+        z = model(data).detach().numpy()
+        plt.plot(z[0].T, c=cmap(idx), 
+                 label=str(par), alpha=alpha,
+                 linewidth=linewidth)
+        if num_repeat > 1:
+            plt.plot(z[1:].T, c=cmap(idx), 
+                     alpha=alpha, linewidth=linewidth)
+            
+    ax = plt.gca()
+    ax.set_facecolor(background)
+    
+    plt.legend(loc='right')
+    plt.show()
+
+
+def plot_neuron_states(model, u, y,
+                       figsize=(8, 6),
+                      alpha=0.5,
+                      apply_activation=False,
+                      plot_by='',
+                      linewidth=1.5):
+    """
+    The weights are normalized by their max absolute value and used to control
+    the alpha parameter of each line.
+
+    Param:
+    plot_by
+        'weight' - Colors neurons with positive weights blue and negative red
+        'adj_weight' - Each neuron is multiplied by the sign of its weight.
+    """
+
+    cmap = plt.cm.get_cmap('brg', model.network_size)
+
+    plt.figure(figsize=figsize)
+    plt.xlabel("Time step")
+    plt.ylabel("Neuron value")
+    if apply_activation:
+        plt.ylabel("Neuron activation")
+    plt.title("")
+
+    z, activity = model(u, visible_activity=True)
+    if apply_activation:
+        activity = model.activation(activity)
+    activity = activity.detach().numpy()
+    activity = activity.squeeze()
+    w = model.w.detach().numpy().squeeze()
+    max_abs_w = np.abs(w).max()
+    w /= max_abs_w
+
+    print(u.shape)
+
+    for i in range(u.size(0)):
+
+        color = cmap(i)
+        act = activity.T[i]
+
+        if plot_by == 'weight':
+            color = 'b'
+            if w[i] < 0: color = 'r'
+
+        if plot_by == 'adj_weight':
+            if w[i] < 0: act *= -1
+            color = 'b'
+            if act[-1] < 0: color = 'r'
+
+        plt.plot(act, alpha=abs(w[i]), c=color, linewidth=linewidth)
+
+
+def plot_network_in_m_i(model, f_in_vec, in_params, 
+                        num_repeat=10, 
+                        figsize=(8,6),
+                        alpha=0,
+                        background='grey',
+                        linewidth=2.5):
+    
+    if alpha==0:
+        alpha = 1/num_repeat
+    
+    cmap = plt.cm.get_cmap('turbo', len(in_params))
+
+    plt.figure(figsize=figsize)
+    plt.xlabel("m component")
+    plt.ylabel("I component")
+    
+    
+    wi = model.wi.detach().numpy().squeeze()
+    m = model.m.detach().numpy().squeeze()
+    
+    wi_dot_wi = wi.dot(wi)
+    m_dot_m = m.dot(m)
+    
+    wi_norm = wi / wi_dot_wi**0.5
+    m_norm = m / m_dot_m**0.5
+    
+    m_dot_wi = m_norm.dot(wi_norm)
+    
+    plt.title("$m \cdot I = {:1.5f}$".format(m_dot_wi))
+    
+    
+    for idx, par in enumerate(in_params):
+        for i in range(num_repeat):
+            u, _ = f_in_vec(par)
+            u = torch.tensor(u, dtype=torch.float32)
+            z, unit_activity = model(u, visible_activity=True)
+            unit_activity = unit_activity.detach().numpy().squeeze()
+
+            wi_comp = (wi @ unit_activity.T) / wi_dot_wi
+            m_comp = (m @ unit_activity.T) / m_dot_m
+            
+            if i == 0:
+                plt.plot(m_comp, wi_comp, 
+                         color=cmap(idx), alpha=alpha, 
+                         label=str(par), linewidth=linewidth)
+            else:
+                plt.plot(m_comp, wi_comp, 
+                         color=cmap(idx), alpha=alpha,
+                         linewidth=linewidth)
+
+            plt.scatter(m_comp[0], wi_comp[0], color='g')
+            plt.scatter(m_comp[-1], wi_comp[-1], color='r')
+            
+    ax = plt.gca()
+    ax.set_facecolor(background)
+    box = ax.get_position()
+    ax.set_position([box.x0, box.y0, box.width * 0.8, box.height])
+
+    # Put a legend to the right of the current axis
+    ax.legend(loc='center left', bbox_to_anchor=(1, 0.5))
+    plt.show()
